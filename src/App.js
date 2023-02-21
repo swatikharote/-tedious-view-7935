3505e9cbaa00b5059c7c95aafce865d81deb3748
kharoteswati <kharoteswati31@gmail.com>
2023-02-21 20:36:54 +0530

    initial steup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Try from './components/try';
+
+function App() {
+  return (
+    <div className="App">
+      <Try />
+    </div>
+  );
+}
+
+export default App;
